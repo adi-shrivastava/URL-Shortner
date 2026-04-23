f1b372630d003b180862fb412599397b5b178e21
Adi Shrivastava <shrivadi0405@gmail.com>
2026-04-23 14:30:09 +0530

    Established connection to mongoDB and added db.js

---
+++ Backend/db.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose");
+const MongoURL="mongodb://localhost:27017/URLShortner";
+mongoose.connect(MongoURL);
+const db=mongoose.connection;
+db.on("connected",()=>{
+    console.log("MongoDB Sever Connected!!");
+})
+db.on("error",()=>{
+    console.log("MongoDB Server Connection Failed!");
+})
+db.on("disconnected",()=>{
+    console.log("MongoDB Sever Disconnected!");
+})
+module.exports=mongoose;
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
-const express=require('express')
-port=3000
+const express=require('express');
+const db=require('./db');
+port=3000;
 const app=express()
 app.get("/",(req,res)=>{
-    console.log("HELLO FROM SERVER!")
+    res.send("Welcome to URL shortner Homepage YESS!!!");
 })
 app.listen(3000,()=>{
-    console.log(`Sever is running on port ${port}`)
+    console.log(`Sever is running on port ${port}`);
 })
