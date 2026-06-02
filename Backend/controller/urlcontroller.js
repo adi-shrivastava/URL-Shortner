b5ca3e9b1f5e3ebbf704e537bc4a4b212fdba862
Adi Shrivastava <shrivadi0405@gmail.com>
2026-06-02 23:19:45 +0530

    Added safety check for URLs in urlcontroller.js added Google Safe Browsing API

--- Backend/controller/urlcontroller.js
+++ Backend/controller/urlcontroller.js
@@ -1,10 +1,23 @@
 const Url = require("../models/urlmodel")
-
-exports.generateShortUrl=(req,res)=>{
+const checkUrlsafety = require("../middleware/safetycheck")
+console.log(checkUrlsafety)
+exports.generateShortUrl=async(req,res)=>{
     try{
         const{url}=req.body
+        // console.log(url)
         if(!url){
             return res.status(400).json({error:"URL is required"})
+        }
+        const response=await checkUrlsafety(url)
+        if(response.matches){ //matches condition by GOOGLE SAFETY CHECK API (DANGEROUS URL)
+            return res.status(400).json({error:"URL is unsafe"})
+        }
+        else{
+            console.log("URL is safe")
+        }
+        const existingurl=await Url.findOne({originalUrl:url})
+        if(existingurl){
+            return res.status(200).json({shortUrl:existingurl.short})
         }
         const shortUrl=Math.random().toString(36).substring(2,8)
         res.status(200).json({shortUrl})
---
+++ Backend/middleware/safetycheck.js
@@ -0,0 +1,37 @@
+const axios = require("axios")
+require('dotenv').config()
+const key = (process.env.GOOGLE_SAFETY_API_KEY)
+// const controller=require('./../controller/urlcontroller')
+async function checkUrlsafety(url) {
+    try {
+        const response = await axios.post(`https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${key}`, {
+            client: {
+                clientId: "urlshortner",
+                clientVersion: "1.0"
+            },
+            threatInfo: {
+                threatTypes: [
+                    "MALWARE",
+                    "SOCIAL_ENGINEERING",
+                    "UNWANTED_SOFTWARE"
+                ],
+                platformTypes: [
+                    "ANY_PLATFORM"
+                ],
+                threatEntryTypes: [
+                    "URL"
+                ],
+                threatEntries: [
+                    {
+                        url: url
+                    }
+                ]
+            }
+        });
+        return response.data;
+    } 
+    catch(err) {
+        res.status(500).json({ error: "Error checking URL safety" });
+    }
+}
+module.exports = checkUrlsafety
