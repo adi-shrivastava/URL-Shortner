dffceed73af253e80d979b02c4ea576f8520a9fc
Adi Shrivastava <shrivadi0405@gmail.com>
2026-06-07 20:53:57 +0530

    Ratelimit is done and linked to routes

--- Backend/middleware/ratelimit.js
+++ Backend/middleware/ratelimit.js
@@ -7,10 +7,13 @@ async function rateLimit(req,res,next){
         if(reqcount===1){
             await RedisClient.expire(ip,60)
         }
-        
+        if(reqcount>100){
+            return res.status(429).json({error:"Too many requests."})
         }
+    }
     catch(err){
         console.error("Error in rate limiting middleware:",err);
+        res.status(500).json({error:"Internal Server Error"})
     }
 }
 module.exports=rateLimit
