00a34ac7047469bbc32d2a37f98efc5e52137fb4
Adi Shrivastava <shrivadi0405@gmail.com>
2026-06-07 18:36:46 +0530

    added expiry to key ratelimit:ip

--- Backend/middleware/ratelimit.js
+++ Backend/middleware/ratelimit.js
@@ -3,7 +3,11 @@ async function rateLimit(req,res,next){
     try{
         const ip=req.ip
         const currentTime=Date.now()
-
+        const reqcount=await RedisClient.incr(ip)
+        if(reqcount===1){
+            await RedisClient.expire(ip,60)
+        }
+        
         }
     catch(err){
         console.error("Error in rate limiting middleware:",err);
