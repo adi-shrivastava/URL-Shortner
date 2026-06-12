f77bd22e7a390beb9ca5c4cd839071e92c628911
Adi Shrivastava <shrivadi0405@gmail.com>
2026-06-12 22:05:39 +0530

    Fixed render port mapping

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const express=require('express');
 const db=require('./db');
 require("dotenv").config();
 const routes=require('./routes/urlroutes');
-const port=5000;
+const PORT = process.env.PORT || 5000;
 const logrequest=(req,res,next)=>{
     console.log(`${req.method} ${req.url} ${new Date().toString()}`);
     next();
@@ -15,6 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(logrequest);
 app.use("/",routes);
-app.listen(5000,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
